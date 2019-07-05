5747166ad8e78e16617bf14160392c42ad804c87
MyskUnion <50514582+MyskUnion@users.noreply.github.com>
2019-07-05 14:55:57 +0300

    Add files via upload

---
+++ Animals2019k.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tyaa.animals;
+
+import org.tyaa.animalslib.Cat;
+import org.tyaa.animalslib.Goldfish;
+
+/**
+ *
+ * @author student
+ */
+public class Animals {
+
+    private static Turtle Turtle;
+
+    /**
+     * @param args the command line arguments
+     * @param Turtle
+     */
+    public static void main(String[] args, Turtle Turtle) {
+        
+        Cat cat = new Cat();
+        cat.breathe();
+        cat.eat();
+        
+        Goldfish g = new Goldfish();
+        g.eat();
+        
+        Turtle = new Turtle()
+        t.crawls();
+        
+        Pig = new Pig
+        p.bathe();
+        
+        Dolfin = new Dolfin
+        d.does();        
+    }
+    
+  System.out.println (Goldfish ":" "I swimming")
+  System.out.println (Cat ":" "I eating")
+  System.out.println (Turtle ":" "I crawlsing")
+  System.out.println (Pig ":" "I swimming in mud")
+  System.out.println (Dolfin ":" "I do tricks")
+
+    private static class t {
+
+        private static void crawls() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public t() {
+        }
+    }
+}
